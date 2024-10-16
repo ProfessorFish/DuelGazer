a449148c152a556c50f076484dff02c07ea9ef23
ProfessorFish <67111659+ProfessorFish@users.noreply.github.com>
2024-10-16 11:57:25 +0100

    Update preload.js

--- preload.js
+++ preload.js
@@ -1,9 +1,7 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
-module.exports = function () {
-  contextBridge.exposeInMainWorld('load', {
+contextBridge.exposeInMainWorld('load', {
     cards: () => ipcRenderer.invoke("loadCards"),
     search: () => ipcRenderer.invoke("loadSearch"),
     decks: () => ipcRenderer.invoke("loadDecks")
-  })
-}
+})
